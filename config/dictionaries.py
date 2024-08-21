5e7a384e3b85f1bf5e412e21f39606f4839f0d08
cranberry314 <andrew.finn@gmail.com>
2024-08-21 13:27:19 -0400

    Add files via upload

---
+++ config/dictionaries.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+"""
+
+# Configuration for dictionaries
+
+DESCRIPTION_TO_CATEGORY = {
+    'LOAN': 'Mortgage', 'TMOBILE': 'Phone', 'SHAWS': 'Groceries',
+    'IRS USATAXPYMT': 'Taxes' 
+    'Withdrawal from Credit Card': 'Credit Card',
+    'Deposit from Work': 'Salary',
+    'Heating': 'Home Services', 'ATM': 'ATM', 'VENMO': 'Merchandise'
+}
+
+CREDIT_DESCRIPTION_TO_CATEGORY = {
+    'DAYCARE: 'Childcare', 'YMCA': 'Exercise',
+    'MARKET BASKET': 'Groceries', 'STOP & SHOP': 'Groceries', 'SHAWS': 'Groceries', 'ALDI': 'Groceries',
+    'WHOLEFDS': 'Groceries', 'HANNAFORD': 'Groceries', 'TRADER JOE': 'Groceries', 'COSTCO WHSE': 'Groceries',
+    'BJS WHOLESALE': 'Groceries', 'WEGMANS': 'Groceries', 'UBER': 'Auto/Transport', 'COSTCO GAS': 'Auto/Transport',
+    'STATE SQUARE GARAGE': 'Auto/Transport', 'NAPA STORE': 'Auto/Transport',
+   'SUNOCO': 'Auto/Transport',
+    'HERTZ': 'Auto/Transport',
+    'Prime Video Channels': 'Entertainment',  'DISNEY': 'Entertainment',
+    'COMCAST': 'Cable', 'LINKEDIN': 'Merchandise'
+    }
---
+++ config/file_paths.py
@@ -0,0 +1,6 @@
+# file_paths.py
+
+# Define file paths
+DATA_PATH = '/path/to/files'
+CREDIT_CARD_FILE = 'credit_card.csv'
+BANK_STATEMENT_FILE = 'bank_statement.csv'
